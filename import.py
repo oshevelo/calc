b6b68a6bd748781e8a5d4854b431113f92aa725e
NMykhailo <nmykhailo@ukr.net>
2019-10-02 11:16:15 +0300

    add import from def day_date / def different_date and as name

---
+++ import.py
@@ -0,0 +1,9 @@
+#import
+from day_date import day_date as day
+from difference_date import difference_date as date
+
+res = day()
+print("Import result :", res)
+
+res2 = date()
+print("Import result :", res2)
